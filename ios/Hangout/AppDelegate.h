93323c8afe048365aa37a2670512798c90f04749
Ayobami <you@example.com>
2021-01-07 21:29:49 +0100

    Success

--- ios/Hangout/AppDelegate.h
+++ ios/Hangout/AppDelegate.h
@@ -1,5 +1,6 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import <FIRApp.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
--- src/component/navigation/app-stack.js
+++ src/component/navigation/app-stack.js
@@ -85,7 +85,7 @@ class AppStack extends Component {
               headerStyle: { backgroundColor: '#7862ff' },
               headerShown: false,
             }}
-            initialRouteName="events">
+            initialRouteName="Splash">
 
             <Stack.Screen name="Splash" component={Splash} />
             <Stack.Screen name="sorting" component={Sorting} />
